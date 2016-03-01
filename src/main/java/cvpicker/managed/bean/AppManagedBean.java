07e52795b1d96420b357d6b5c5f2cec628e781aa
avakovdavid <avakov@hotmail.fr>
2016-03-01 16:47:57 +0100

    Remove useless code

--- src/main/java/cvpicker/managed/bean/AppManagedBean.java
+++ src/main/java/cvpicker/managed/bean/AppManagedBean.java
@@ -4,28 +4,20 @@
  */
 package cvpicker.managed.bean;
 
-import cvpicker.hibernate.Cv;
-import cvpicker.hibernate.Friend;
 import cvpicker.hibernate.HibernateUtil;
 import cvpicker.hibernate.Privacy;
-import cvpicker.hibernate.User;
 import java.io.Serializable;
-import java.security.MessageDigest;
-import java.util.List;
 import javax.annotation.PostConstruct;
-import javax.faces.application.FacesMessage;
-import javax.faces.context.FacesContext;
 import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
-import org.hibernate.criterion.Restrictions;
 
 /**
  *
  * @author DAVID
  */
 public class AppManagedBean implements Serializable{
-
+    
     @PostConstruct
     public void init() {
         Session session = HibernateUtil.getSessionFactory().openSession();
